0acda043efc87604f0f8753363d6d690fdf99c4f
headkn <byeongwook.seo@gmail.com>
2018-03-04 21:04:51 +0900

    make player movable

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+    private float xValue;
+    private float yValue;
+
+	// Use this for initialization
+	void Start () {
+        speed = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xValue = Input.GetAxis("Horizontal");
+        yValue = Input.GetAxis("Vertical");
+        Vector3 newPos = player.transform.position + new Vector3(xValue * speed * Time.deltaTime, 0.0f, yValue * speed * Time.deltaTime);
+        player.transform.position = newPos;
+	}
+}
