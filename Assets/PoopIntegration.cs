0fa238f197843a62647c212c683086ef58dec9fc
jaehosung <jaehossung@gmail.com>
2018-03-04 23:08:06 +0900

    Poop prototype1

---
+++ Assets/PoopIntegration.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework.Internal;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PoopIntegration : MonoBehaviour
+{
+
+	private bool done = false;
+	public GameObject Poop;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.tag =="poop"&&done ==false)
+		{
+			done = true;
+			//#I need to Fix this problem
+			Transform thisObject = this.gameObject.transform;
+			Transform colObject = col.gameObject.transform;
+			Destroy(this);
+			Destroy(col.gameObject);
+
+			float size = Mathf.Sqrt(thisObject.localScale.x * thisObject.localScale.x +
+			                        colObject.localScale.x * colObject.localScale.x);
+			
+			
+			if (this.gameObject.transform.position.magnitude > col.gameObject.transform.position.magnitude)
+			{
+				GameObject newObject;
+				newObject = Instantiate(Poop, (colObject.position+thisObject.position)/2, Quaternion.identity);
+				newObject.transform.localScale = new Vector3(size,0.3f,size);
+			}
+			
+		}
+	}
+}
--- Assets/Scripts/PoopManager.cs
+++ Assets/Scripts/PoopManager.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
-public class GameManager : MonoBehaviour {
+public class PoopManager : MonoBehaviour
+{
+	public GameObject Poop;
 
 	// Use this for initialization
 	void Start () {
-
+		Debug.Log(Poop.transform.localScale);
+		for (int i = 0; i < 20; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				for (int k = 0; k < 3; k++)
+				{
+					if(Random.value > 0.5)
+						Instantiate(Poop, new Vector3(j*3, i * 20 + 10, k * 3), Quaternion.identity);
+				}
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
+	//When Meet ==> Delete both and make a new object
+	//Poop.transform.localScale += new Vector3(1f,0,0);	
+	
 }
