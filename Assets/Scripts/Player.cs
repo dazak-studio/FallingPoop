3cef1329b4ccf95eb7d583adc988a2341a35550f
headkn <byeongwook.seo@gmail.com>
2018-03-04 21:36:15 +0900

    make land and player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 }
