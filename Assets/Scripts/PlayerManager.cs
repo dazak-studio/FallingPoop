9c6bea5026cb9f4cad0305be959e718b9aff138b
oh3vci <oh3vci@gmail.com>
2018-03-04 23:32:39 +0900

    Enable the player movement

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed = 6f;
+	// public bool isEquipedShovel = false;
+
+
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+
+	void Awake ()
+	{
+		playerRigidbody = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update ()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
 	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0f, v);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	/*
+	void EquipShovel (bool isEquipedS)
+	{
+		Input.GetButtonDown ("Equip");
+	}
+	*/
 }
