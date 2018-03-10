a8aa46c85acbb423210af1ea5e4a38a03c525108
headkn <byeongwook.seo@gmail.com>
2018-03-10 16:56:32 +0900

    assign ScoreText to UIManager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
+
+    public Text scoreText;
 
 	// Use this for initialization
 	void Start () {
