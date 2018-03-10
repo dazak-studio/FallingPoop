930187e3a26b4780248b5cc9eacaa405c381b6be
headkn <byeongwook.seo@gmail.com>
2018-03-10 17:30:22 +0900

    make UIManager and Time Flow

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,14 +6,62 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
 
     public Text scoreText;
+    private float leftTime;
+    private bool isPlaying;
 
 	// Use this for initialization
 	void Start () {
-		
+        DoGameReady();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CheckLeftTime();
+
+        // Test Code by Key Z, X, C
+        /*
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            DoGameReady();
+        }
+        else if(Input.GetKeyDown(KeyCode.X))
+        {
+            DoGameStart();
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            DoGameOver();
+        }
+        */
 	}
+
+    // call when you want to ready game
+    public void DoGameReady()
+    {
+        isPlaying = false;
+        leftTime = 0.0f;
+        scoreText.text = "Ready";
+    }
+
+    // call when you want to start game
+    public void DoGameStart()
+    {
+        isPlaying = true;
+    }
+
+    // call when you want to do gameover
+    public void DoGameOver()
+    {
+        isPlaying = false;
+        scoreText.text = "Game Over";
+    }
+
+    private void CheckLeftTime()
+    {
+        if (isPlaying)
+        {
+            leftTime += Time.deltaTime;
+            scoreText.text = "You Survived for " + (int)leftTime + " seconds!";
+        }
+    }
 }
