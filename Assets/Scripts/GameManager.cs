f9eac4373fa9b3d11777f4fcb4ea2087c345c1fe
headkn <byeongwook.seo@gmail.com>
2018-03-11 10:05:41 +0900

    assign managers to gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public GameObject playerManager;
+    public GameObject poopManager;
+    public GameObject uiManager;
+
 	// Use this for initialization
 	void Start () {
-		
+        //playerManager = GameObject.FindGameObjectWithTag("PlayerManager");
+        //uiManager = GameObject.FindGameObjectWithTag("UIManager");
+
 	}
 	
 	// Update is called once per frame
