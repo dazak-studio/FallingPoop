07ac9e12a0bfe85291496f5f498edec028eed468
oh3vci <oh3vci@gmail.com>
2018-03-18 10:34:19 +0900

    Change player movement logic from set position to translate

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,56 +4,66 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+  public float moveSpeed = 12f;
+  public bool isEquipedShovel = false;
 
-	public float speed = 6f;
-	public bool isEquipedShovel = false;
+  Vector3 lookDirection;
 
-	Vector3 movement;
-	Rigidbody playerRigidbody;
-	MeshRenderer playerMeshRenderer;
+  Vector3 movement;
+  Rigidbody playerRigidbody;
+  MeshRenderer playerMeshRenderer;
 
-	void Awake ()
-	{
-		playerRigidbody = GetComponent<Rigidbody> ();
-		playerMeshRenderer = GetComponent<MeshRenderer> ();
-	}
+  void Awake ()
+  {
+    playerRigidbody = GetComponent<Rigidbody> ();
+    playerMeshRenderer = GetComponent<MeshRenderer> ();
+  }
 
-	void Update ()
-	{
-		float h = Input.GetAxisRaw ("Horizontal");
-		float v = Input.GetAxisRaw ("Vertical");
+  void Update ()
+  {
+    if (Input.GetKey (KeyCode.W) ||
+        Input.GetKey (KeyCode.A) ||
+        Input.GetKey (KeyCode.S) ||
+        Input.GetKey (KeyCode.D))
+    {
+      float h = Input.GetAxisRaw ("Horizontal");
+      float v = Input.GetAxisRaw ("Vertical");
+      
+      Move (h, v);
+    }
 
-		Move (h, v);
+    if (Input.GetKeyDown (KeyCode.Return))
+    {
+      ToggleShovel(isEquipedShovel);
+    }
+  }
 
-        /*
-		if (Input.GetKeyDown (KeyCode.Return))
-		{
-			ToggleShovel(isEquipedShovel);
-		}
-        */
-	}
+  void Move (float h, float v)
+  {
+    float moveSpeed = this.moveSpeed;
 
-	void Move (float h, float v)
-	{
-		movement.Set (h, 0f, v);
+    if (isEquipedShovel) {
+      moveSpeed = 5f;
+    }
 
-		movement = movement.normalized * speed * Time.deltaTime;
+    // Set player look direction
+    lookDirection = h * Vector3.right + v * Vector3.forward;
+    this.transform.rotation = Quaternion.LookRotation (lookDirection);
 
-		playerRigidbody.MovePosition (transform.position + movement);
-	}
+    this.transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+  }
 
-	void ToggleShovel (bool isEquipedShovel)
-	{
-		this.isEquipedShovel = !this.isEquipedShovel;
+  void ToggleShovel (bool isEquipedShovel)
+  {
+    this.isEquipedShovel = !this.isEquipedShovel;
 
-		if (isEquipedShovel)
-		{
-			playerMeshRenderer.material.color = Color.red;
-		}
-		else
-		{
-			playerMeshRenderer.material.color = Color.white;
-		}
-	}
-
+    if (isEquipedShovel)
+    {
+      playerMeshRenderer.material.color = Color.white;
+    }
+    else
+    {
+      playerMeshRenderer.material.color = Color.red;
+    }
+  }
 }
